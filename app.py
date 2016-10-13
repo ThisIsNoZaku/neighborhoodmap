0e6c85ddc54dc957a0b10d466048f7b04f2b31e3
Damien <damienmarble@gmail.com>
2016-10-13 15:37:21 -0700

    Reformatted app.py according to pep8.

--- app.py
+++ app.py
@@ -13,51 +13,51 @@ app = Flask(__name__)
 
 # Reading credentials from json https://github.com/Yelp/yelp-python
 with io.open('yelp_secret.json') as cred:
-	creds = json.load(cred)
-	yelp_auth = Oauth1Authenticator(**creds)
-	client = Client(yelp_auth)
+    creds = json.load(cred)
+    yelp_auth = Oauth1Authenticator(**creds)
+    client = Client(yelp_auth)
 
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/yelp")
 def search():
-	params = {}
-	request_args = request.args.lists()
-	for param in request_args:
-		params[param[0]] = param[1][0]
-	lat = params.pop('latitude')
-	lng = params.pop('longitude')
-	search = client.search_by_coordinates(lat, lng, **params)
-	businesses = []
-	# The objects returned by the Yelp client aren't serializable 
-	# by default
-	for business in search.businesses:
-		b = {
-			'name' : business.name,
-			'id' : business.id,
-			'display_phone' : business.display_phone,
-			'image_url' : business.image_url,
-			'location' : {
-				'address' : business.location.address,
-				'coordinate' : {
-					'latitude' : business.location.coordinate.latitude,
-					'longitude' : business.location.coordinate.longitude
-				},
-				'display_address' : business.location.display_address
-			},
-			'mobile_url' : business.mobile_url,
-			'rating_img_url_large' : business.rating_img_url_large,
-			'rating_img_url_small' : business.rating_img_url_small,
-			'rating_img_url' : business.rating_img_url,
-			'snippet_text' : business.snippet_text
-		}
-		businesses.append(b)
+    params = {}
+    request_args = request.args.lists()
+    for param in request_args:
+        params[param[0]] = param[1][0]
+    lat = params.pop('latitude')
+    lng = params.pop('longitude')
+    search = client.search_by_coordinates(lat, lng, **params)
+    businesses = []
+    # The objects returned by the Yelp client aren't serializable
+    # by default
+    for business in search.businesses:
+        b = {
+            'name': business.name,
+            'id': business.id,
+            'display_phone': business.display_phone,
+            'image_url': business.image_url,
+            'location': {
+                'address': business.location.address,
+                'coordinate': {
+                    'latitude': business.location.coordinate.latitude,
+                    'longitude': business.location.coordinate.longitude
+                },
+                'display_address': business.location.display_address
+            },
+            'mobile_url': business.mobile_url,
+            'rating_img_url_large': business.rating_img_url_large,
+            'rating_img_url_small': business.rating_img_url_small,
+            'rating_img_url': business.rating_img_url,
+            'snippet_text': business.snippet_text
+        }
+        businesses.append(b)
 
-	return jsonify({'businesses' :businesses})
+    return jsonify({'businesses': businesses})
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
